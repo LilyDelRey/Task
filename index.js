6f5b870a65a78995be21bcb3b470f910ac5c1a71
latifa <latifa.mahmoud13@gmail.com>
2017-10-29 17:07:56 +0200

    draft

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+$('.nav > li').hover(function(){
+    $(this).parent().css('background-color', '#62D1A2');
+});
+$('.nav > li').mouseout(function(){
+    $(this).parent().css('background-color', '#4BA0A4');
+});
